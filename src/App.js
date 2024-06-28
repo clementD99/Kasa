7ad26c3fe031d81a9bfa36cdf90ab37a7d929d11
Clément Desmur <clement.desmur@gmail.com>
2024-06-28 18:29:59 +0200

    Modifications mineures dans le code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css';       // Pour que React fonctionne correctement mettre le css, images en haut
+import logo from './images/logo.png';
+import React from 'react';
+                                    
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="App">
+      <header class="App-header">
+        <img src={logo} class="App-logo" alt="logo de Kasa"></img>
+        
       </header>
     </div>
   );
