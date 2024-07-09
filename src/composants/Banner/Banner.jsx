b10911a27f9b470186ec38f72b0ba3e54b80a85b
Clément Desmur <clement.desmur@gmail.com>
2024-07-09 15:29:31 +0200

    MAJ pour banner afin qu'il puisse être réutilisable

--- src/composants/Banner/Banner.jsx
+++ src/composants/Banner/Banner.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-import imgAccueil from "../../images/imgAccueil.png";
-
-function Banner() {
+function Banner({ image, text }) {
   return (
     <div className="banner">
-      <img src={imgAccueil} alt="Image d'accueil" className="banner-img"/>
-      <span className="banner-text">Chez vous, partout et ailleurs</span>
+      <img src={image} alt="Banner" className="banner-img" />
+      <span className="banner-text">{text}</span>
     </div>
   );
 }
 
 export default Banner;
+
+// function Banner() {
+//   return (
+//     <div className="banner">
+//       <img src={imgAccueil} alt="Image d'accueil" className="banner-img"/>
+//       <span className="banner-text">Chez vous, partout et ailleurs</span>
+//     </div>
+//   );
+// }
+
+//----- Code de l'ancien banner s'il faut le réutiliser -----//
