aa583da131ab5887b571f97ccb488274f3fb2934
Clément Desmur <clement.desmur@gmail.com>
2024-07-09 15:28:38 +0200

    Modifs mineures dans les cards

--- src/composants/Cards/Cards.jsx
+++ src/composants/Cards/Cards.jsx
@@ -1,1 +1,22 @@
 import "../Cards/cards.scss";
+
+function Cards() {
+    return (
+        <div className="cards-gallery">
+            <div className="cards-list">
+                <a href="#">Image</a>
+            </div>
+            <div className="cards-list">
+                <a href="#">Image</a>
+            </div>
+            <div className="cards-list">
+                <a href="#">Image</a>
+            </div>
+            <div className="cards-list">
+                <a href="#">Image</a>
+            </div>
+        </div>
+    )
+}
+
+export default Cards; 
