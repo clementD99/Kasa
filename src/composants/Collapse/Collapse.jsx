10333399c94704556dc62a29abb8443357cda330
Clément Desmur <clement.desmur@gmail.com>
2024-07-11 14:50:13 +0200

    Ajout de code pour le contenu dans "à propos"

--- src/composants/Collapse/Collapse.jsx
+++ src/composants/Collapse/Collapse.jsx
@@ -1,11 +1,21 @@
-import collapse from "../../donnees/collapse.json";
 import "../Collapse/collapse.scss";
 import React, { useState } from "react";
 
-function Collapse ({ title, content }) {
-    const [isOpen, setIsOpen] = useState(false);
+function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleCollapse = () => {
-        setIsOpen(!isOpen);
-      };      
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse-barre">
+      <div onClick={toggleCollapse}>
+        {title}
+      </div>
+      {isOpen && <div className="collapse-contenu">{content}</div>}
+    </div>
+  );
 }
+
+export default Collapse;
