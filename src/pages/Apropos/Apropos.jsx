4cd44fa68770205232d2aa22b32cb7fb32c82f11
Clément Desmur <clement.desmur@gmail.com>
2024-07-11 14:51:18 +0200

    Ajout de classes pour les collapses

--- src/pages/Apropos/Apropos.jsx
+++ src/pages/Apropos/Apropos.jsx
@@ -1,50 +1,24 @@
-// import { Link } from "react-router-dom";
-import Header from "../../composants/Header/Header";
 import Banner from "../../composants/Banner/Banner";
-import Footer from "../../composants/Footer/Footer";
 import imgApropos from "../../images/imgApropos.png";
+import Collapse from "../../composants/Collapse/Collapse";
+import CollapseData from "../../donnees/collapseData.json";
 
-// function Apropos() {
-//   return (
-//       <div className="header">
-//         <header className="header-elements">
-//           <img src={imgApropos} className="header-logo" alt="logo de Kasa" />
-//           <ul className="header-nav">
-//             <li>
-//               <Link to="/accueil">Accueil</Link>
-//             </li>
-//             <li>
-//               <Link to="/">À propos</Link>
-//             </li>
-//           </ul>
-//         </header>
-//       </div>
-//   );
-// }
-
-// function Apropos() {
-//   return (
-//       <div className="header">
-//         {/* <Header /> */}
-//         <main>
-//           {/* <Banner /> */}
-//           <img src={imgApropos} title="image" className="banner-img"></img>
-//         </main>
-//         <Footer />
-//       </div>
-//   );
-// }
-
-function Apropos () {
+function Apropos() {
   return (
     <div className="apropos">
-      <Header />
-      <main>
-        <Banner image={imgApropos} title="image"/>
-      </main>
-      <Footer/>
+      <Banner image={imgApropos} title="image" />
+      <div className="collapse">
+        <div className="collapse-barre">
+        <i class="fa-solid fa-chevron-up"></i>
+          <div className="collapse-titre">
+          {CollapseData.map((item) => (
+            <Collapse key={item.id} title={item.title} content={item.content} />
+          ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Apropos;
