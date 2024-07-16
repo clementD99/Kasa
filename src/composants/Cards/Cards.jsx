fdc0de63e0f99b10aa206be3d24c4e5a33052fbc
Clément Desmur <clement.desmur@gmail.com>
2024-07-16 15:04:12 +0200

    Changement mineur

--- src/composants/Cards/Cards.jsx
+++ src/composants/Cards/Cards.jsx
@@ -1,22 +1,24 @@
 import "../Cards/cards.scss";
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
 
 function Cards() {
-    return (
-        <div className="cards-gallery">
-            <div className="cards-list">
-                <a href="#">Image</a>
-            </div>
-            <div className="cards-list">
-                <a href="#">Image</a>
-            </div>
-            <div className="cards-list">
-                <a href="#">Image</a>
-            </div>
-            <div className="cards-list">
-                <a href="#">Image</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="cards-gallery">
+      <div className="cards-list">
+        <a href="#">Image</a>
+      </div>
+      <div className="cards-list">
+        <a href="#">Image</a>
+      </div>
+      <div className="cards-list">
+        <a href="#">Image</a>
+      </div>
+      <div className="cards-list">
+        <a href="#">Image</a>
+      </div>
+    </div>
+  );
 }
 
-export default Cards; 
+export default Cards;
