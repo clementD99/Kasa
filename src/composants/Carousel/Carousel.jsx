476c3075ba6266a3b0159a3f871db06a2050ad84
Clément Desmur <clement.desmur@gmail.com>
2024-07-23 14:58:54 +0200

    Initialisation du carrousel

---
+++ src/composants/Carousel/Carousel.jsx
@@ -0,0 +1,11 @@
+import './carousel.scss';
+
+const Carousel = () => {
+    return (
+        <div class="carousel">
+            <p>CAROUSEL</p>
+        </div>
+    )
+};
+
+export default Carousel;
