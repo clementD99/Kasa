3854bc4722c406ede7744fa70b48a044ed34061d
Clément Desmur <clement.desmur@gmail.com>
2024-07-23 14:53:30 +0200

    Code pour naviguer sur les logements

--- src/composants/Cards/Cards.jsx
+++ src/composants/Cards/Cards.jsx
@@ -1,22 +1,50 @@
 import "../Cards/cards.scss";
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Cards() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("./logements.json");
+        if (!response.ok) {
+          throw new Error("Erreur lors de la récupération des données");
+        }
+        const result = await response.json();
+
+        setData(result);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Le tableau vide signifie que cet effet ne se déclenche qu'une fois après le montage du composant
+
+  if (isLoading) {
+    return <div>Chargement...</div>;
+  }
+
+  if (error) {
+    return <div>Erreur : {error.message}</div>;
+  }
+
   return (
     <div className="cards-gallery">
-      <div className="cards-list">
-        <a href="#">Image</a>
-      </div>
-      <div className="cards-list">
-        <a href="#">Image</a>
-      </div>
-      <div className="cards-list">
-        <a href="#">Image</a>
-      </div>
-      <div className="cards-list">
-        <a href="#">Image</a>
-      </div>
+      {data.map((item, index) => (
+        <div className="cards-list" key={index}>
+            <Link to={`/logement/${item.id}`}>
+              <img src={item.cover} alt={`Image ${index}`} />
+            </Link>
+          <span className="cards-list-titre">{item.title}</span>
+        </div>
+      ))}
     </div>
   );
 }
