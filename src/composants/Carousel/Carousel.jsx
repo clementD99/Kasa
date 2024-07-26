43b503cc97a9b51454ec354c3d296f7519931ddc
Clément Desmur <clement.desmur@gmail.com>
2024-07-26 15:50:14 +0200

    Création de code pour faire défiler les images

--- src/composants/Carousel/Carousel.jsx
+++ src/composants/Carousel/Carousel.jsx
@@ -1,11 +1,39 @@
-import './carousel.scss';
+import React, { useState } from "react";
+import "../../composants/Carousel/carousel.scss";
+import flecheGauche from "../../images/carrousel/flecheGauche.png";
+import flecheDroite from "../../images/carrousel/flecheDroite.png";
 
-const Carousel = () => {
-    return (
-        <div class="carousel">
-            <p>CAROUSEL</p>
-        </div>
-    )
+
+const Carousel = ({ pictures }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <div className="carousel">
+      <div onClick={handlePrev} className="carousel-button">
+        <img src={flecheGauche} alt="Précédent" />
+      </div>
+      <img
+        src={pictures[currentIndex]}
+        alt={`Logement ${currentIndex + 1}`}
+        className="carousel-image"
+      />
+      <div onClick={handleNext} className="carousel-button">
+        <img src={flecheDroite} alt="Suivant" />
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
