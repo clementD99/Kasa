342f892d3d75e623f04add539349bd9916abcac6
Clément Desmur <clement.desmur@gmail.com>
2024-07-30 16:15:46 +0200

    Correction mineure

--- src/composants/Collapse/Collapse.jsx
+++ src/composants/Collapse/Collapse.jsx
@@ -1,7 +1,7 @@
 import "../Collapse/collapse.scss";
 import React, { useState } from "react";
 
-function Collapse({ title, content }) {
+function Collapse({ title, content, }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleCollapse = () => {
@@ -12,7 +12,9 @@ function Collapse({ title, content }) {
     <div className="collapse-barre">
       <div className="collapse-titre" onClick={toggleCollapse}>
         {title}
-        <i className={`fa-solid ${isOpen ? 'fa-chevron-down' : 'fa-chevron-up'}`}></i>
+        <i
+          className={`fa-solid ${isOpen ? "fa-chevron-down" : "fa-chevron-up"}`}
+        ></i>
       </div>
       {isOpen && <div className="collapse-contenu">{content}</div>}
     </div>
