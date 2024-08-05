30573af2f1eec6d72c3a630b64e6f075abc65f27
Clément Desmur <clement.desmur@gmail.com>
2024-08-05 16:04:38 +0200

    Suppression d'un commentaire

--- src/composants/Banner/Banner.jsx
+++ src/composants/Banner/Banner.jsx
@@ -8,14 +8,3 @@ function Banner({ image, text }) {
 }
 
 export default Banner;
-
-// function Banner() {
-//   return (
-//     <div className="banner">
-//       <img src={imgAccueil} alt="Image d'accueil" className="banner-img"/>
-//       <span className="banner-text">Chez vous, partout et ailleurs</span>
-//     </div>
-//   );
-// }
-
-//----- Code de l'ancien banner s'il faut le réutiliser -----//
