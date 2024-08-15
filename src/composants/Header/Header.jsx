7c91e55ec44a815bbec92278cfea4e854e5575a6
Clément Desmur <clement.desmur@gmail.com>
2024-08-15 15:28:49 +0200

    Ajout de useLocation pour la barre sous accueil et à propos

--- src/composants/Header/Header.jsx
+++ src/composants/Header/Header.jsx
@@ -1,17 +1,19 @@
 import "./header.scss";
 import logo from "../../images/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation(); // Pour la barre en dessous soit de accueil ou à propos
+  
   return (
     <div className="header">
       <header className="header-elements">
         <img src={logo} className="header-logo" alt="logo de Kasa" />
         <ul className="header-nav">
-          <li>
+          <li className={location.pathname === '/' ? 'active' : ''}>
             <Link to="/">Accueil</Link>
           </li>
-          <li>
+          <li className={location.pathname === '/apropos' ? 'active' : ''}>
             <Link to="/apropos">À propos</Link>
           </li>
         </ul>
